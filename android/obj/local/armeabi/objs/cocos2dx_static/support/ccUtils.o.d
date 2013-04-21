2af2d958a84dc9456051d2f36b892e87c7130707
Brandon Dorris <brandon@orbotix.com>
2013-04-21 12:22:56 -0600

    Removing files that shouldn't be tracked in version control
    It's generally a good idea to ignore any compiled output so that there are no machine-dependent files that could cause issues for others.
    
    e.g. in .gitignore
    **/obj
    **/bin

--- android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/alex/cocos2d-2.0-x-2.0.2/discgroove/proj.android/../../cocos2dx/support/ccUtils.cpp \
- /Users/alex/cocos2d-2.0-x-2.0.2/discgroove/proj.android/../../cocos2dx/support/ccUtils.h
-
-/Users/alex/cocos2d-2.0-x-2.0.2/discgroove/proj.android/../../cocos2dx/support/ccUtils.h:
