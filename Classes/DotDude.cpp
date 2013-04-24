7bcfe12c47aac5cee6553a0f0517c4f4429bf3c1
Alex S <alex@bold-it.com>
2013-04-23 18:14:44 -0700

    using updated graphics

--- Classes/DotDude.cpp
+++ Classes/DotDude.cpp
@@ -12,7 +12,24 @@ DotDude* DotDude::initWithSize(int s, int h)
 {
     DotDude *dotDude = new DotDude();
     dotDude->initWithFile("blank.png");
-    dotDude->setTextureRect(CCRect(0, 0, s, s));
+
+    CCSprite *shipCover = new CCSprite();
+    shipCover->initWithFile("dot.png");
+    shipCover->setPosition(ccp(s/2 + 2 , s/2 + 2));
+    float shipCoverScale = s / shipCover->getContentSize().height ;
+    dotDude->setScale(shipCoverScale);
+    dotDude->addChild(shipCover);
+
+    CCSprite *shadow = new CCSprite();
+    shadow->initWithFile("shadow.png");
+//    shadow->setPosition(ccp(s/2 , s/2));
+    float shadowScale = s / shadow->getContentSize().height;
+    shadow->setScale(shadowScale);
+
+    dotDude->setTexture( shadow->getTexture() );
+    dotDude->setTextureRect( shadow->getTextureRect() );
+
+//    dotDude->setTextureRect(CCRect(0, 0, s, s));
     ccColor3B dotColor = {255,255,255};
     dotDude->setColor(dotColor);
     dotDude->setHp(h);
